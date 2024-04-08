52a5c97bd10eb5d558e20b493756ab8fbf1cd40f
LoogCP <lp18883370076@gmail.com>
2024-04-08 21:36:09 +0800

    Update diy-iw23.sh

--- diy-iw23.sh
+++ diy-iw23.sh
@@ -9,6 +9,16 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/fish/g' package/base-files/files/etc/passwd
 # TTYD 自动登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+
 # 修改frpc
 rm -rf feeds/luci/applications/luci-app-frpc
 git clone https://github.com/yhl452493373/luci-app-frpc.git package/luci-app-frpc
