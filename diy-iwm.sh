ffb87870cf16f54e1a56b7f68e3cddd19402f58c
LoogCP <lp18883370076@gmail.com>
2024-06-28 22:53:49 +0800

    修改脚本结构，使用默认zerotier

--- diy-iwm.sh
+++ diy-iwm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+./scripts/feeds update -a
 
 # 修改默认IP
 sed -i 's/192.168.1.1/10.10.1.1/g' package/base-files/files/bin/config_generate
@@ -6,11 +7,9 @@ sed -i 's/192.168.1.1/10.10.1.1/g' package/base-files/files/bin/config_generate
 # 更改默认 Shell 为 fish
 sed -i 's/\/bin\/ash/\/usr\/bin\/fish/g' package/base-files/files/etc/passwd
 
-# 修改frpc、zerotier
+# 修改frpc
 rm -rf feeds/luci/applications/luci-app-frpc
-rm -rf feeds/luci/applications/luci-app-zerotier
 git clone https://github.com/yhl452493373/luci-app-frpc.git package/luci-app-frpc
-git clone --depth=1 https://github.com/zhengmz/luci-app-zerotier.git feeds/luci/applications/luci-app-zerotier
 
 
 # 添加额外插件
@@ -27,30 +26,18 @@ git clone --depth=1 -b master https://github.com/kiddin9/luci-theme-edge package
 #svn export https://github.com/haiibo/packages/trunk/luci-theme-opentomcat package/luci-theme-opentomcat
 #svn export https://github.com/haiibo/packages/trunk/luci-theme-netgear package/luci-theme-netgear
 
-# 更改 Argon 主题背景
-#cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-
-
-
-# 在线用户
-#svn export https://github.com/haiibo/packages/trunk/luci-app-onliner package/luci-app-onliner
-#sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/emortal/default-settings/files/99-default-settings
-#sed -i '$i uci commit nlbwmon' package/emortal/default-settings/files/99-default-settings
-#chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
-
 # x86 型号只显示 CPU 型号
-#sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/emortal/autocore/files/x86/autocore
+sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/emortal/autocore/files/x86/autocore
 
 # 修改本地时间格式
-#sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/emortal/autocore/files/*/index.htm
+sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/emortal/autocore/files/*/index.htm
 
 # 修改版本为编译日期
-#date_version=$(date +"%y.%m.%d")
-#orig_version=$(cat "package/emortal/default-settings/files/99-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-#sed -i "s/${orig_version}/R${date_version} by LoogCP/g" package/emortal/default-settings/files/99-default-settings
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/emortal/default-settings/files/99-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by LoogCP/g" package/emortal/default-settings/files/99-default-settings
 
 # 修复 hostapd 报错
 #cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
 
-./scripts/feeds update -a
 ./scripts/feeds install -a -f
