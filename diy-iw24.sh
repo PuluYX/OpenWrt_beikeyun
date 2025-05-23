89ed4efa90c75d078c7932ac43061da7680dcd70
PuluYX <131853963+PuluYX@users.noreply.github.com>
2025-05-23 11:26:45 +0800

    diy-iw24.sh

--- diy-iw24.sh
+++ diy-iw24.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 修改默认IP
-sed -i 's/192.168.1.1/10.10.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.124.88/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 fish
 sed -i 's/\/bin\/ash/\/usr\/bin\/fish/g' package/base-files/files/etc/passwd
